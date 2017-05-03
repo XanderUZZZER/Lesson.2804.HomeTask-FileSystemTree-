dbb086ab157f322cca0502630eeb19d9ee208425
Oleksandr Lukhanin <xander_uzzzer@mail.ru>
2017-05-03 17:25:18 +0300

    0305171725

---
+++ Lesson.2804.HomeTask(FileSystemTree)/FileSystemTree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson._2804.HomeTask_FileSystemTree_
+{
+    class FileSystemTree
+    {
+        string path;//= Directory.GetCurrentDirectory();
+        public DirectoryInfo dI;// = new DirectoryInfo(path);
+        DirectoryInfo[] dirs;
+        DirectoryInfo[] temp;
+        bool hasSubFolders = true;
+        StringBuilder pad = new StringBuilder();        
+
+        public FileSystemTree()
+        {
+            path = Directory.GetCurrentDirectory();
+            dI = new DirectoryInfo(path);
+            dirs = dI.GetDirectories();
+            temp = dirs;
+            //pad.Append('-');
+        }
+        public void ShowDirs()
+        {
+            while (hasSubFolders)
+            {
+                ShowDir();
+            }
+
+        }
+        public void ShowDir()
+        {
+            foreach (var dir in temp)
+            {
+                Console.WriteLine(dir.Name);
+                if (dir.GetDirectories() != null)
+                {
+                    hasSubFolders = true;
+                    temp = dir.GetDirectories();
+                }
+                else
+                {
+                    hasSubFolders = false;
+                }
+            }
+        }
+        public void ShowDirRecurs()
+        {            
+            foreach (var dir in temp)
+            {
+                
+                Console.WriteLine(pad + dir.Name);
+                if (dir.GetDirectories() != null)
+                {pad.Append('-');
+                    temp = dir.GetDirectories();
+                    ShowDirRecurs();                    
+                }
+            }
+
+            if (pad.Length != 0)
+                pad.Remove(0, 1);
+        }
+    }
+}
--- Lesson.2804.HomeTask(FileSystemTree)/Program.cs
+++ Lesson.2804.HomeTask(FileSystemTree)/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lesson._2804.HomeTask_FileSystemTree_
 {
@@ -10,6 +11,43 @@ namespace Lesson._2804.HomeTask_FileSystemTree_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Lesson 2804 HomeTask\n");
+
+            string path = Directory.GetCurrentDirectory();
+            FileSystemTree fst = new FileSystemTree();
+
+            //fst.ShowDirs();
+            fst.ShowDirRecurs();
+
+            //DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Xander_UZZZER\Documents\Visual Studio 2015\Projects\Lesson.2804.HomeTask(FileSystemTree)\Lesson.2804.HomeTask(FileSystemTree)\bin\Debug");
+            //Console.WriteLine("============Список каталогов=============");
+            //foreach (var item in dir.GetDirectories())
+            //{
+            //    Console.WriteLine(item.Name);
+            //    Console.WriteLine("==Список подкаталогов==");
+            //    foreach (var it in item.GetDirectories())
+            //        Console.WriteLine(it.Name);
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("==============Список файлов==============");
+            //foreach (var item in dir.GetFiles())
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            //using (var fileStream = File.OpenWrite("file.txt"))
+            //{
+            //    using (var writer = new StreamWriter(fileStream))
+            //    {
+            //        for (int i = 0; i < 50; i++)
+            //        {
+            //            writer.WriteLine(i);
+            //        }
+            //    }
+            //}
+
+            Console.WriteLine("End");
+            Console.ReadLine();
         }
     }
 }
